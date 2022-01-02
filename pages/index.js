895abe3fb15f68c1e1423a7546c85c06ab596996
ABlued <53801395+ABlued@users.noreply.github.com>
2022-01-02 14:52:28 +0900

    Design : 로그인 폼 뷰 개발

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,55 @@
 import Head from 'next/head';
 import Box from '@mui/material/Box';
-import styles from '../styles/Home.module.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import styles from '../styles/Home.module.scss';
+import { useState } from 'react';
 
 export default function Home() {
+  const [userID, setUserID] = useState('');
+  const [userPW, setUserPW] = useState('');
+
+  const changeUserID = (e) => {
+    setUserID(e.target.value);
+  };
+  const changeUserPW = (e) => {
+    setUserPW(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className={styles.container}>
       <Head>
         <title>로그인 페이지</title>
       </Head>
 
-      <Box component="form" className={styles.loginForm}>
-        <h1>로그인 페이지</h1>
+      <Box className={styles.loginForm}>
+        <h1>Touch Class</h1>
+        <Box component="form" onSubmit={handleSubmit}>
+          <div className={styles.loginFormInput}>
+            <TextField
+              className={styles.idFiled}
+              label="아이디"
+              variant="outlined"
+              onChange={changeUserID}
+            />
+            <TextField
+              className={styles.pwFiled}
+              label="비밀번호"
+              variant="outlined"
+              onChange={changeUserPW}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{ display: 'inline' }}
+            >
+              login
+            </Button>
+          </div>
+        </Box>
       </Box>
     </div>
   );
